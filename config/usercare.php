a9535aba406ade7ea283f185ce80b949291c97c1
Seddor Ephraim <seddorephraim7@gmail.com>
2021-11-18 23:33:34 +0100

    Add user id route param as config

--- config/usercare.php
+++ config/usercare.php
@@ -1,5 +1,10 @@
 <?php
 
 return [
-    
+    'user'  => [
+        /*
+        | The name of the parameter you set in your web.php or api.php to get user's ID
+        */
+        'id_route_param'    => 'id',
+    ],
 ];
