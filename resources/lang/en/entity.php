927a05cef9120e54f84247e94fecda179f3301f0
Seddor Ephraim <seddorephraim7@gmail.com>
2021-11-20 13:29:58 +0100

    Remove messages.php and attributes key in enntity.php

--- resources/lang/en/entity.php
+++ resources/lang/en/entity.php
@@ -1,29 +1,7 @@
 <?php
 
 return [
-    'account_type'              => 'Account Type|Account Types',
-    'user'                      => 'User|Users',
-    'user_profile'              => 'User Profile|User Profiles',
-
-    'attributes'                =>  [
-        'name'                  => 'Name',
-        'username'              => 'Username',
-        'last_name'             => 'Last name',
-        'first_name'            => 'First name',
-        'email'                 => 'Email address',
-        'phone_number'          => 'Phone Number',
-        'slug'                  => 'Slug',
-        'description'           => 'Description',
-        'password'              => 'Password',
-        'old_password'          => 'Old Password',
-        'new_password'          => 'New Password',
-        'confirm_password'      => 'Confirm Password',
-        'confirm_new_password'  => 'Confirm New Password',
-        'avatar'                => 'Avatar',
-        'cover_picture'         => 'Cover Picture',
-        'background_picture'    => 'Background Picture',
-        'user_profile'          => 'User Profile',
-        'user_infos'            => 'User Infos',
-        'profile'               => 'Profile',
-    ],
+    'account_type'  => 'Account Type|Account Types',
+    'user'          => 'User|Users',
+    'user_profile'  => 'User Profile|User Profiles',
 ];
