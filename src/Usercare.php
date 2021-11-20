94dd47635abad6dd203d309fdf27f8bec55be073
Seddor Ephraim <seddorephraim7@gmail.com>
2021-11-20 13:08:33 +0100

    Feat: Add methods to get any entity single data resource

--- src/Usercare.php
+++ src/Usercare.php
@@ -2,7 +2,44 @@
 
 namespace Mawuekom\Usercare;
 
+use Illuminate\Database\Eloquent\Model;
+
 class Usercare
 {
-    // Build your next great package.
+    /**
+     * Get entity resource by field and value
+     * 
+     * @param string $entity
+     * @param string $attribute
+     * @param string $value
+     * @param bool   $inTrashed
+     * @param array  $columns
+     * 
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function getEntityByField(string $entity, $field, $value = null, $inTrashed = false, $columns = ['*']): Model
+    {
+        $data = app(config('usercare.'.$entity.'.model')) ->where($field, '=', $value);
+
+        return ($inTrashed)
+                    ? $data ->withTrashed() ->first($columns)
+                    : $data ->first($columns);
+    }
+
+    /**
+     * Get entity resource by id
+     * 
+     * @param string        $entity
+     * @param int|string    $id
+     * @param bool          $inTrashed
+     * @param array         $columns
+     * 
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function getEntityById(string $entity, $id, $inTrashed = false, $columns = ['*'])
+    {
+        $key = resolve_key('usercare', config('usercare.user.slug'), $id, $inTrashed);
+
+        return $this ->getEntityByField($entity, $key, $id, $inTrashed, $columns);
+    }
 }
