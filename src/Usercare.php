b7bba2736793aa20cd39e088c82c54625597be2c
Seddor Ephraim <seddorephraim7@gmail.com>
2021-11-20 13:30:52 +0100

    Refactor: Rename  variable to

--- src/Usercare.php
+++ src/Usercare.php
@@ -9,7 +9,7 @@ class Usercare
     /**
      * Get entity resource by field and value
      * 
-     * @param string $entity
+     * @param string $entity_slug
      * @param string $attribute
      * @param string $value
      * @param bool   $inTrashed
@@ -17,9 +17,9 @@ class Usercare
      * 
      * @return \Illuminate\Database\Eloquent\Model
      */
-    public function getEntityByField(string $entity, $field, $value = null, $inTrashed = false, $columns = ['*']): Model
+    public function getEntityByField(string $entity_slug, $field, $value = null, $inTrashed = false, $columns = ['*']): Model
     {
-        $data = app(config('usercare.'.$entity.'.model')) ->where($field, '=', $value);
+        $data = app(config('usercare.'.$entity_slug.'.model')) ->where($field, '=', $value);
 
         return ($inTrashed)
                     ? $data ->withTrashed() ->first($columns)
@@ -29,17 +29,17 @@ class Usercare
     /**
      * Get entity resource by id
      * 
-     * @param string        $entity
+     * @param string        $entity_slug
      * @param int|string    $id
      * @param bool          $inTrashed
      * @param array         $columns
      * 
      * @return \Illuminate\Database\Eloquent\Model
      */
-    public function getEntityById(string $entity, $id, $inTrashed = false, $columns = ['*'])
+    public function getEntityById(string $entity_slug, $id, $inTrashed = false, $columns = ['*'])
     {
         $key = resolve_key('usercare', config('usercare.user.slug'), $id, $inTrashed);
 
-        return $this ->getEntityByField($entity, $key, $id, $inTrashed, $columns);
+        return $this ->getEntityByField($entity_slug, $key, $id, $inTrashed, $columns);
     }
 }
