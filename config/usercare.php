14c14179c83b250a7e82fd2b17dd1d91fbd87f2e
Seddor Ephraim <seddorephraim7@gmail.com>
2021-11-20 04:16:18 +0100

    Feat: Allow to config profiles table and model

--- config/usercare.php
+++ config/usercare.php
@@ -6,5 +6,25 @@ return [
         | The name of the parameter you set in your web.php or api.php to get user's ID
         */
         'id_route_param'    => 'id',
+
+
+        /*
+        | User's profile config
+        */
+
+        'profile'       => [
+            'enabled'       => true,
+            'model'         => null,
+        
+            /*
+            | User's profile table config
+            */
+
+            'table'         => [
+                'name'              => env('USERCARE_PROFILES_DATABASE_TABLE', 'profiles'),
+                'primary_key'       => env('USERCARE_PROFILES_DATABASE_TABLE_PRIMARY_KEY', 'id'),
+                'user_foreign_key'  => env('USERCARE_PROFILES_DATABASE_TABLE_USER_FOREIGN_KEY', 'user_id'),
+            ],
+        ],
     ],
 ];
