4bdb9f8dbbd4cbbe7a158157a8354de896e24188
Seddor Ephraim <seddorephraim7@gmail.com>
2021-11-20 14:11:16 +0100

    Feat: Add account type store and update request validation rules

---
+++ src/Http/Requests/StoreAccountTypeRequest.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Mawuekom\Usercare\Http\Requests;
+
+use Illuminate\Validation\Rule;
+use Mawuekom\RequestCustomizer\FormRequestCustomizer;
+use Mawuekom\RequestSanitizer\Sanitizers\Capitalize;
+use Mawuekom\Usercare\DataTransferObjects\AccountTypeDTO;
+use Mawuekom\Usercare\Services\AccountTypeService;
+
+class StoreAccountTypeRequest extends FormRequestCustomizer
+{
+    /**
+     * @var \Mawuekom\Usercare\Services\AccountTypeService
+     */
+    protected $accountTypeService;
+
+    /**
+     * Create new form request instance.
+     *
+     * @param \Mawuekom\Usercare\Services\AccountTypeService $useaccountTypeServicerService
+     */
+    public function __construct(AccountTypeService $accountTypeService)
+    {
+        parent::__construct();
+        $this ->accountTypeService = $accountTypeService;
+    }
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        $accountTypesTable = config('usercare.account_type.table.name');
+
+        return [
+            'name'          => 'required|string|max:255',
+            'slug'          => [
+                'required', 'string', 'max:255',
+                Rule::unique($accountTypesTable, 'slug')
+            ],
+            'description'   => 'string',
+        ];
+    }
+
+    /**
+     * Get sanitizers defined for form input
+     *
+     * @return array
+     */
+    public function sanitizers(): array
+    {
+        return [
+            'name' => [
+                Capitalize::class,
+            ]
+        ];
+    }
+
+    /**
+     * Build and return a DTO
+     *
+     * @return \Mawuekom\Usercare\DataTransferObjects\AccountTypeDTO
+     */
+    public function toDTO(): AccountTypeDTO
+    {
+        return new AccountTypeDTO([
+            'name'          => $this ->name,
+            'slug'          => $this ->slug,
+            'descripton'    => $this ->description,
+        ]);
+    }
+
+    /**
+     * Fulfill the update account type request
+     *
+     * @return array
+     */
+    public function fulfill(): array
+    {
+        return $this ->accountTypeService ->create($this ->toDTO());
+    }
+}
