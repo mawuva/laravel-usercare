948219e360823a56c778994a18172e29d65c834a
Seddor Ephraim <seddorephraim7@gmail.com>
2021-11-22 16:53:44 +0100

    Refactor: Update functions params order in helpers.php

--- src/helpers.php
+++ src/helpers.php
@@ -2,15 +2,15 @@
 
 if ( ! function_exists('success_response')) {
     /**
-     * Generate username from random string
+     * Return array of response
      *
-     * @param string $message
      * @param array|null|Object $data
+     * @param string $message
      * @param int $code
      * 
      * @return array
      */
-    function success_response(string $message = 'Action performed successfully', $data = null, int $code = 200): array {
+    function success_response($data = null, string $message = 'Action performed successfully', int $code = 200): array {
         return [
             'code'      => $code,
             'status'    => 'success',
@@ -22,15 +22,15 @@ if ( ! function_exists('success_response')) {
 
 if ( ! function_exists('failure_response')) {
     /**
-     * Generate username from random string
+     * Return array of response
      *
-     * @param string $message
      * @param array|null|Object $data
+     * @param string $message
      * @param int $code
      * 
      * @return array
      */
-    function failure_response(string $message = 'Action attempted failed', $data = null, int $code = 0): array {
+    function failure_response($data = null, string $message = 'Action attempted failed', int $code = 0): array {
         return [
             'code'      => $code,
             'status'    => 'failed',
