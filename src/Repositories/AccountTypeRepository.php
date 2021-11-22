80230ca5ba836c132f85d78a997a223f6beeea72
Seddor Ephraim <seddorephraim7@gmail.com>
2021-11-22 17:19:27 +0100

    Feat: Provide repositories with QueryLogic Trait to implement commons data quering requests

--- src/Repositories/AccountTypeRepository.php
+++ src/Repositories/AccountTypeRepository.php
@@ -3,9 +3,12 @@
 namespace Mawuekom\Usercare\Repositories;
 
 use Mawuekom\Repository\Eloquent\BaseRepository;
+use Mawuekom\Repository\Logics\QueryLogic;
 
 class AccountTypeRepository extends BaseRepository
 {
+    use QueryLogic;
+    
     /**
      * Get the model on which works
      * 
@@ -22,6 +25,46 @@ class AccountTypeRepository extends BaseRepository
      * @return array
      */
     public function searchableFields()
+    {
+        return ['name', 'slug'];
+    }
+
+    /**
+     * Columns on which filterig will be done
+     * 
+     * @return array
+     */
+    public function filterableFields(): array
+    {
+        return ['name', 'slug'];
+    }
+
+    /**
+     * Determine by which property the results collection will be ordered
+     * 
+     * @return array
+     */
+    public function sortableFields(): array
+    {
+        return ['name', 'slug', 'created_at'];
+    }
+
+    /**
+     * Determine the relation that will be load on the resulting model
+     * 
+     * @return array
+     */
+    public function includableRelations(): array
+    {
+        return ['users'];
+    }
+
+    /**
+     * Define a couple fields that will be fetch to reduce the overall size of your SQL query
+     * 
+     * @return array
+     */
+    public function selectableFields(): array
     {
         return ['name', 'slug'];
     }
--- src/Repositories/UserRepository.php
+++ src/Repositories/UserRepository.php
@@ -3,9 +3,12 @@
 namespace Mawuekom\Usercare\Repositories;
 
 use Mawuekom\Repository\Eloquent\BaseRepository;
+use Mawuekom\Repository\Logics\QueryLogic;
 
 class UserRepository extends BaseRepository
 {
+    use QueryLogic;
+    
     /**
      * Get the model on which works
      * 
@@ -23,6 +26,46 @@ class UserRepository extends BaseRepository
      */
     public function searchableFields()
     {
-        return ['name', 'email'];
+        return ['name', 'first_name', 'email', 'phone_number'];
+    }
+
+    /**
+     * Columns on which filterig will be done
+     * 
+     * @return array
+     */
+    public function filterableFields(): array
+    {
+        return ['name', 'first_name', 'email', 'phone_number'];
+    }
+
+    /**
+     * Determine by which property the results collection will be ordered
+     * 
+     * @return array
+     */
+    public function sortableFields(): array
+    {
+        return ['name', 'first_name', 'email', 'phone_number', 'created_at'];
+    }
+
+    /**
+     * Determine the relation that will be load on the resulting model
+     * 
+     * @return array
+     */
+    public function includableRelations(): array
+    {
+        return ['profile', 'accountType'];
+    }
+
+    /**
+     * Define a couple fields that will be fetch to reduce the overall size of your SQL query
+     * 
+     * @return array
+     */
+    public function selectableFields(): array
+    {
+        return ['name', 'first_name', 'email', 'phone_number'];
     }
 }
